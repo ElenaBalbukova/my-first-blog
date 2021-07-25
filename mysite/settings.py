0ff1a84fa5bc3f9c3664f4582944002f61a2e4ef
ElenaBalbukova <evbalbukova@gmail.com>
2021-07-25 17:56:56 +0300

    Added secure, comment, PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'elena',
     }
 }
 
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
