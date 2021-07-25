c37ff714f2080b7e0b4ce0a196eef6edc6fb7bfb
ElenaBalbukova <evbalbukova@gmail.com>
2021-07-25 19:45:32 +0300

    Changed the bd.sqlite3 for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangogirls',
-        'USER': 'elena',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
